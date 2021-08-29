d0f61956effc1b7824e8d09e0708623704041052
guser210 <gilv210@yahoo.com>
2021-08-29 13:32:59 -0700

    Update SSD1306.py

--- MicorPython/SSD1306.py
+++ MicorPython/SSD1306.py
@@ -2,7 +2,6 @@ import utime
 from machine import Pin
 from machine import I2C
 
-
 class SSD:
     def __init__(self, port=0, freq=400000, scl=17, sda=16):
         self.address = 60
@@ -14,7 +13,9 @@ class SSD:
             print(device)
             
     def sendCommand(self, cmd, data):
-        command = [0b11000000 & cmd, data]
+        if type(data) != list: data = [data]
+        
+        command = [0b11000000 & cmd] + data
         self.dev.writeto(self.address, bytearray(command))
         
     def powerUp(self):
@@ -25,10 +26,11 @@ class SSD:
     def clear(self, on=255):
         for page in range(0xB0, 0xb8):
             self.sendCommand(0b10000000, page)
-            self.sendCommand(0b00000000, 0x00)
-            self.sendCommand(0b00000000, 0x10)
+            self.sendCommand(0b00000000, [0x00,0x10])
+           
+            mem = [on for i in range(0,130)]
+            self.sendCommand(0b01000000,mem)
             
-            self.dev.writeto(self.address, bytearray([0b01000000] + [on for i in range(0,129)]))
     def plotPixel(self, x=10, y=10, on=1):
         
         xl = int(x & 0x0f)
@@ -39,12 +41,10 @@ class SSD:
         page = int(( y - (y%y))/7) | 0xb0
         
         self.sendCommand(0b10000000, page)
-        self.sendCommand(0b00000000, xl)
-        self.sendCommand(0b00000000, xh)
+        self.sendCommand(0b00000000, [xl,xh])
         
         self.sendCommand(0b01000000,pixel)
-                
-    
+                    
 display = SSD()
 
 display.printDevices()
@@ -53,16 +53,13 @@ display.clear(on=0)
 
 display.plotPixel(64,32)
 
-
 x = 30
 y = 30
 
 incx = 1
 incy = 1
 
-
-while True:
-    
+while True: 
     display.plotPixel(x,y,on = 0)
     x += incx
     y += incy
@@ -74,11 +71,3 @@ while True:
     
     utime.sleep(0.005)
     
-
-
-
-
-
-
-
-
